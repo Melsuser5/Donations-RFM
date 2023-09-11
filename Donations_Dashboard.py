42c9b4ec22105b4a09e02248b3ba9610692a9279
Sam Harvey <70475300+Melsuser5@users.noreply.github.com>
2023-09-11 16:30:54 +1000

    Update Donations_Dashboard.py

--- Donations_Dashboard.py
+++ Donations_Dashboard.py
@@ -10,7 +10,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/Melsuser5/Donations-RFM/main
 custom_palette = ["#5a8eb8", "#5ab874", "#bf3636", "#f08922", "#8146d4", "#e3528e", "#2a9ac7"]
 
 df["Segment"] = df["overall_score"].astype(str)
-fig = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='Segment',width=1600, height=800)
+fig_all = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='Segment',width=1600, height=800)
 log_fig = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='Segment', log_x=False,log_y=True,log_z=True, width=1600, height=800,color_continuous_scale=custom_palette)
 
 
